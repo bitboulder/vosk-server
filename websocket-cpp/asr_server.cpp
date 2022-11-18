835cf4f0b5d19a0f71d492c7acadb6e35682a662
Daniel Zoba <daniel.sobe@sorben.com>
2022-11-18 14:18:02 +0100

    add hack to filter out messages for setting sampling rate

--- websocket-cpp/asr_server.cpp
+++ websocket-cpp/asr_server.cpp
@@ -141,6 +141,14 @@ public:
         {
             return Chunk{vosk_recognizer_final_result(rec_), true};
         }
+        // dirty hack, clients send their sampling rate this way, but
+        // this is not mapped to an API in Vosk, so filter it out here
+        else if ((len < 100) && (strstr(message, "sample_rate") != NULL))
+        {
+        	// careful, the buffer is sometimes not null-terminated!
+        	std::cout << message << "\n";
+        	return Chunk{vosk_recognizer_partial_result(rec_), false};
+        }
         else if (vosk_recognizer_accept_waveform(rec_, message, len))
         {
             return Chunk{vosk_recognizer_result(rec_), false};
